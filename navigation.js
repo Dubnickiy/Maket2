bd623286b0fd46a08c4478e4da66fac827c23fe6
Dubnickiy <dubnickiy.zhenya@gmail.com>
2022-01-15 20:41:03 +0200

    first commit

---
+++ navigation.js
@@ -0,0 +1,14 @@
+$('.scrollto a').on('click', function() {
+
+    let href = $(this).attr('href');
+
+    $('html, body').animate({
+        scrollTop: $(href).offset().top
+    }, {
+        duration: 800,    
+        easing: "linear"  
+    });
+
+    return false;
+});
+
---
+++ slider.js
@@ -0,0 +1,10 @@
+jQuery(function( $ ) {
+  $('.one-time').slick({
+    dots: true,
+    arrows: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true
+});
+});
